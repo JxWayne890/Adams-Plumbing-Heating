16d40c1a96d2dba66b38a548c0c761b76d73a025
JxWayne890 <JxWayne890@gmail.com>
2025-01-10 23:19:50 -0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-    import { defineConfig } from 'vite';
-    import react from '@vitejs/plugin-react';
-
-    export default defineConfig({
-      base: '/Adams-Plumbing-Heating/', // Replace with your repo name
-      plugins: [react()],
-    });
